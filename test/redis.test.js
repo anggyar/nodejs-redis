e411639d8809b56517446cee7ff14bd9271ca029
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-07-25 02:13:21 +0700

    add connection to redis, test ping connection to redist, and check if redis support string

---
+++ test/redis.test.js
@@ -0,0 +1,34 @@
+import Redis from "ioredis";
+
+describe("redis", () => {
+    let redis = null;
+
+    beforeEach(async () => {
+        redis = new Redis({
+            host: "localhost",
+            port: 6379,
+            db: 0,
+        });
+    });
+
+    afterEach(async () => {
+        await redis.quit();
+    });
+
+    it("should can ping", async () => {
+        const pong = await redis.ping();
+        expect(pong).toBe("PONG");
+    });
+
+    it("should support string", async () => {
+        await redis.setex("name", 2, "Anggy");
+
+        let name = await redis.get("name");
+        expect(name).toBe("Anggy");
+
+        // sleep 5 seconds
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        name = await redis.get("name");
+        expect(name).toBeNull();
+    });
+});
